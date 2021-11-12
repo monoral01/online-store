59ba2acbbe8c770b389358c1fbceb96c40c151f9
monoral01 <chebot.kirill.r@gmail.com>
2021-11-12 21:43:13 +0300

    fix node bugs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import React, { useState } from 'react';
 import './styles/App.css';
-
 function App() {
-  return <div className="App">БМБА</div>;
+  return (
+    <div className="App">
+      <div></div>
+    </div>
+  );
 }
 
 export default App;
